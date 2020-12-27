880b523c562482d6f2eae32a04dfebac06f3d06c
近藤輝 <kondouakira@kondouakiranoMacBook-Pro.local>
2020-12-28 01:30:06 +0900

    move Yolo Class in train.py to yolo.py and remove useless import in train.py

--- yolov1/train.py
+++ yolov1/train.py
@@ -1,12 +1,9 @@
 import os
 from pathlib import Path
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torchvision import models
-from pytorch_lightning.core.lightning import LightningModule
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning import Trainer
 import hydra
@@ -14,50 +11,7 @@ import hydra
 from net import resnet50
 from loss import yoloLoss
 from dataloader import yoloDataset
-from visualize import save_res_im
-
-
-class Yolo(LightningModule):
-
-    def __init__(self, net, loss_func, train_root, val_root, target_file, lr, epochs, pred_classes,
-            batch_size, im_save):
-        super().__init__()
-        self.net = net
-        self.criterion = loss_func
-        self.train_root = train_root
-        self.val_root = val_root,
-        self.target_file = target_file
-        self.lr = lr
-        self.epochs = epochs
-        self.pred_classes = pred_classes
-        self.batch_size = batch_size
-
-        # self.im_save = Path("/res/res_im").mkdir(exist_ok=True, parents=True)
-        self.im_save = Path("/res/res_im")
-        self.im_save.mkdir(exist_ok=True, parents=True)
-        self.now_epoch = 1
-    
-    def forward(self, x):
-        return self.net(x)
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = self.criterion(logits, y)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        x, y = batch
-        logits = self(x)
-        loss = self.criterion(logits, y)
-        save_res_im(x, logits, y, "res_{}.jpg".format(self.now_epoch), str(self.im_save))
-        self.now_epoch += 1
-        self.log("val_loss", loss)
-        return loss
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.net.parameters(), lr=self.lr)
-
+from yolo import Yolo
 
 
 @hydra.main("./config.yml")
---
+++ yolov1/yolo.py
@@ -0,0 +1,48 @@
+from pytorch_lightning.core.lightning import LightningModule
+import torch
+
+from pathlib import Path
+
+from visualize import save_res_im
+
+
+class Yolo(LightningModule):
+
+    def __init__(self, net, loss_func, train_root, val_root, target_file, lr, epochs, pred_classes,
+            batch_size, im_save):
+        super().__init__()
+        self.net = net
+        self.criterion = loss_func
+        self.train_root = train_root
+        self.val_root = val_root,
+        self.target_file = target_file
+        self.lr = lr
+        self.epochs = epochs
+        self.pred_classes = pred_classes
+        self.batch_size = batch_size
+
+        # self.im_save = Path("/res/res_im").mkdir(exist_ok=True, parents=True)
+        self.im_save = Path("/res/res_im")
+        self.im_save.mkdir(exist_ok=True, parents=True)
+        self.now_epoch = 1
+    
+    def forward(self, x):
+        return self.net(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.criterion(logits, y)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.criterion(logits, y)
+        save_res_im(x, logits, y, "res_{}.jpg".format(self.now_epoch), str(self.im_save))
+        self.now_epoch += 1
+        self.log("val_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.net.parameters(), lr=self.lr)
